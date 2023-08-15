7515d62b35b39c9aa7cebf1fb06d3603678b2a98
Sadia-Naaz <139625425+Sadia-Naaz@users.noreply.github.com>
2023-08-15 14:26:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function decimalToBinary(num) {
+function decimalToBinary(decimal,binary) {
  let output=[];
-	for(let i =num.value;i>0;i=(parseInt)(i/2)){
+	for(let i =decimal.value;i>0;i=(parseInt)(i/2)){
 		output.push(i%2);
 	}
-	
+	binary.value=output.reverse.toString();
   
 }
  decimalToBinary();
